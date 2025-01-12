c380df9d107ce30fba8c0b9f0325414643cbe7b8
nacho-kr <wldyd0078@gmail.com>
2025-01-12 13:16:46 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-  <div id="kyong">
+  <div id="jiyong">
     <FlowerRain />
     <IntroMovie />
     <Live />
@@ -226,7 +226,7 @@ button {
 * {
   box-sizing: border-box;
 }
-#kyong {
+#jiyong {
   font-family: "Cafe24Oneprettynight", sans-serif;
   max-width: 460px;
   margin: 0 auto;
